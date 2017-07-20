b7982326431eb16ef7e7e6f2fea67fccbdab404c
Trevor Spiteri <trevor.spiteri@um.edu.mt>
2017-07-20 10:41:57 +0200

    fix doc link typos

--- src/gmp.rs
+++ src/gmp.rs
@@ -1029,7 +1029,7 @@ extern "C" {
 pub unsafe fn mpq_numref(op: mpq_ptr) -> mpz_ptr {
     (&mut (*op).num) as mpz_ptr
 }
-/// Constant version of [`mpq_numref`](extern "C" fn.mpq_numref.html).
+/// Constant version of [`mpq_numref`](fn.mpq_numref.html).
 #[inline]
 pub unsafe extern "C" fn mpq_numref_const(op: mpq_srcptr) -> mpz_srcptr {
     (&(*op).num) as mpz_srcptr
@@ -1039,7 +1039,7 @@ pub unsafe extern "C" fn mpq_numref_const(op: mpq_srcptr) -> mpz_srcptr {
 pub unsafe fn mpq_denref(op: mpq_ptr) -> mpz_ptr {
     (&mut (*op).den) as mpz_ptr
 }
-/// Constant version of [`mpq_denref`](extern "C" fn.mpq_denref.html).
+/// Constant version of [`mpq_denref`](fn.mpq_denref.html).
 #[inline]
 pub unsafe extern "C" fn mpq_denref_const(op: mpq_srcptr) -> mpz_srcptr {
     (&(*op).den) as mpz_srcptr
