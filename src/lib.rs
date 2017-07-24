01ba4c59c747c9613b6c7939d58cdca4825d1296
Trevor Spiteri <trevor.spiteri@um.edu.mt>
2017-07-24 15:59:57 +0200

    version 1.0.6

--- src/lib.rs
+++ src/lib.rs
@@ -191,7 +191,7 @@
 //! [sys mpfr]: mpfr/index.html
 
 #![warn(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/gmp-mpfr-sys/1.0.5/",
+#![doc(html_root_url = "https://docs.rs/gmp-mpfr-sys/1.0.6/",
        test(attr(deny(warnings))))]
 
 pub mod gmp;
