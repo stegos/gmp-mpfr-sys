d9d3f6ee2d84d5cfb1e4432e7e68e3ee631d831d
Trevor Spiteri <trevor.spiteri@um.edu.mt>
2017-12-28 08:52:00 +0100

    touch c libraries less

--- build.rs
+++ build.rs
@@ -35,9 +35,9 @@ use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
-const GMP_DIR: &'static str = "gmp-6.1.2-slim";
-const MPFR_DIR: &'static str = "mpfr-4.0.0-slim";
-const MPC_DIR: &'static str = "mpc-1.1-rc1-slim";
+const GMP_DIR: &'static str = "gmp-6.1.2-c";
+const MPFR_DIR: &'static str = "mpfr-4.0.0-c";
+const MPC_DIR: &'static str = "mpc-1.1-rc1-c";
 
 fn main() {
     let src_dir = PathBuf::from(cargo_env("CARGO_MANIFEST_DIR"));
--- unpack.sh
+++ unpack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script untars gmp, mpfr, mpc and slims them down.
+# This script untars gmp, mpfr, mpc and tweaks them a little.
 
 set -e
 
@@ -24,48 +24,60 @@ MPCVERP="$MPCVER"
 MPCTAR="$TARDIR/mpc-$MPCVER.tar.gz"
 MPCPATCH="$TARDIR/mpc-$MPCVERP-allpatches"
 
+# GMP
+# 1. Remove ChangeLog, doc/*.info*
+# 2. Remove demos section in configure
+# 3. Remove doc*Makefile, demos*Makefile from ac_config_files in configure
+# 4. Remove doc and demos from SUBDIRS in Makefile.in
 tar xf "$GMPTAR"
-mv gmp-$GMPVER gmp-$GMPVERP-slim
-cd gmp-$GMPVERP-slim
+mv gmp-$GMPVER gmp-$GMPVERP-c
+cd gmp-$GMPVERP-c
 if [ -f "$GMPPATCH" ]; then
 	patch -N -Z -p1 < "$GMPPATCH" > /dev/null
 fi
-rm -r ac*.m4 ChangeLog configure.ac demos
-find doc -name \*.tex\* -o -type f -delete
+rm ChangeLog doc/*.info*
 sed -i.rm~ '/Configs for demos/,/Create config.m4/{//!d}' configure
 sed -i.rm~ '/^ac_config_files=/s/[^ ]*\(doc\|demos\)[^ ]*.Makefile//g' \
     configure
 sed -i.rm~ '/^SUBDIRS = /s/doc\|demos//g' Makefile.in
 cd ..
 
+# MPFR
+# 1. Remove ChangeLog, doc/*.info, doc/*.html
+# 2. Remove doc*Makefile, mpfr.pc from ac_config_files in configure
+# 3. Remove doc from SUBDIRS in Makefile.in
+# 4. Remove $(pkgconfig_DATA) from DATA in Makefile.in
 tar xf "$MPFRTAR"
-mv mpfr-$MPFRVER mpfr-$MPFRVERP-slim
-cd mpfr-$MPFRVERP-slim
+mv mpfr-$MPFRVER mpfr-$MPFRVERP-c
+cd mpfr-$MPFRVERP-c
 if [ -f "$MPFRPATCH" ]; then
 	patch -N -Z -p1 < "$MPFRPATCH" > /dev/null
 fi
-rm -r ac*.m4 ChangeLog configure.ac m4
-find doc -name \*.tex\* -o -type f -delete
+rm ChangeLog doc/*.info doc/*.html
 sed -i.rm~ '/^ac_config_files=/s/\([^ ]*doc[^ ]*.Makefile\|mpfr.pc\)//g' configure
 sed -i.rm~ '/^SUBDIRS = /s/doc//g' Makefile.in
 sed -i.rm~ '/^DATA = /s/\$(pkgconfig_DATA)//g' Makefile.in
 cd ..
 
+# MPC
+# 1. Remove ChangeLog, doc/*.info
+# 2. Remove doc*Makefile from ac_config_files in configure
+# 3. Remove doc from SUBDIRS in Makefile.in
 tar xf "$MPCTAR"
-mv mpc-$MPCVER mpc-$MPCVERP-slim
-cd mpc-$MPCVERP-slim
+mv mpc-$MPCVER mpc-$MPCVERP-c
+cd mpc-$MPCVERP-c
 if [ -f "$MPCPATCH" ]; then
 	patch -N -Z -p1 < "$MPCPATCH" > /dev/null
 fi
-rm -rf ac*.m4 ChangeLog configure.ac m4
-find doc -name \*.tex\* -o -type f -delete
+rm ChangeLog doc/*.info
 sed -i.rm~ '/^ac_config_files=/s/[^ ]*doc[^ ]*.Makefile//g' configure
 sed -i.rm~ '/^SUBDIRS = /s/doc//g' Makefile.in
-chmod u+w * doc/*
 cd ..
 
-find *-slim -name Makefile.am | xargs rm
-for m in $(find *-slim -name Makefile.in); do
+# Finally
+# 1. Remove Makefile:...esac sections from all Makefile.in
+# 2. Remove all .rm~ files left over by sed
+for m in $(find *-c -name Makefile.in); do
 	sed -i.rm~ '/Makefile:/,/esac/d' $m
 done
-find *-slim -name \*.rm~ | xargs rm
+find *-c -name \*.rm~ -delete
